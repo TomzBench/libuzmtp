4cbd46306092364aaeca466ced690c3abdc90907
thomas <=>
2016-08-18 16:47:40 -0400

    test fixture verifies zmq+tls hello world!

--- tests/index.js
+++ tests/index.js
@@ -4,26 +4,63 @@
 
 var tls = require('tls'),
   zmq = require('zmq'),
+  q = require('q'),
+  net = require('net'),
   fs = require('fs');
 
-(function() {
-  if (module.parent) return;
-  start();
-})();
+function Queue() {};
+Queue.prototype.listen = function(port, cb) {
+  var zsock = zmq.socket("router");
+  return q.ninvoke(zsock, "bind", "tcp://*:" + port).then(function() {
+    if ((cb) && (typeof(cb) == 'function')) cb(zsock);
+    return zsock;
+  }).catch(function(e) {
+    console.log(e);
+  });
+}
+
+start();
 
 function start() {
-  var server = tls.createServer({
-    key: fs.readFileSync("./server_key.pem"),
-    cert: fs.readFileSync("./server_cert.pem")
-  }, function(s) {
-    s.on("data", function(data) {
-      console.log(data);
+  var queue = new Queue();
+  queue.listen(23401).then(function(sock) {
+    tls.createServer({
+      key: fs.readFileSync("./server_key.pem"),
+      cert: fs.readFileSync("./server_cert.pem")
+    }, function(s) {
+      s.on("data", function(data) {
+        c.write(data, "binary");
+      });
+      s.on("error", function(error) {
+        console.log(error);
+      });
+      s.on("end", function() {
+        console.log("server terminated");
+      });
+
+      var c = net.connect({
+        port: 23401
+      });
+      c.on("data", function(data) {
+	s.write(data, "binary");
+      });
+      c.on("error", function(e) {
+        console.log(error);
+      });
+      c.on("end", function() {
+        console.log("client terminated");
+      });
+
+    }).listen(23400);
+
+    sock.on("message", function(rid, msg) {
+      console.log(msg.toString());
     });
-    s.on("error", function(e) {
+    sock.on("error", function(e) {
       console.log(e);
     });
-    s.on("end", function() {
-      console.log("end");
-    });
-  }).listen(23400);
+  }).done();
 }
+//
+//
+//
