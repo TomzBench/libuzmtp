03198bdd8a7ab4951c9e4a8a3419f4b4d462700b
thomas <thomas@altronix.com>
2022-04-18 19:46:16 -0400

    clang format

--- apps/demo/src/main.c
+++ apps/demo/src/main.c
@@ -1,7 +1,9 @@
-#include <zephyr.h>
 #include <uzmtp/uzmtp.h>
+#include <zephyr.h>
 
-void main(void) {
+void
+main(void)
+{
     while (1) {
         k_msleep(1000);
     }
--- include/uzmtp/uzmtp_types.h
+++ include/uzmtp/uzmtp_types.h
@@ -11,20 +11,22 @@
 extern "C" {
 #endif
 
-#include "uzmtp/uzmtp_platform.h"
+#include "uzmtp_platform.h"
 
 typedef struct uzmtp_dealer__s uzmtp_dealer_s;
 typedef struct uzmtp_msg__s uzmtp_msg_s;
 typedef void uzmtp_connection;
 
-typedef enum {
+typedef enum
+{
     UZMTP_MSG_MORE = 1,
     UZMTP_MSG_LARGE = 2,
     UZMTP_MSG_COMMAND = 4
 } EUZMTP_MSG;
 
 // State machine
-typedef enum {
+typedef enum
+{
     UZMTP_NULL = 0,
     UZMTP_CONNECT_WANT_GREETING = 1,
     UZMTP_CONNECT_WANT_READY,
@@ -33,7 +35,8 @@ typedef enum {
     UZMTP_RECV_BODY
 } EUZMTP_STATE;
 
-typedef enum {
+typedef enum
+{
     UZMTP_ERROR_VERSION = 0,
     UZMTP_ERROR_MEMORY,
     UZMTP_ERROR_SEND,
@@ -46,7 +49,8 @@ typedef int (*uzmtp_recv_fn)(uzmtp_dealer_s*, uint32_t);
 typedef void (*uzmtp_error_fn)(uzmtp_dealer_s*, EUZMTP_ERROR);
 
 // Context callbacks
-typedef struct {
+typedef struct
+{
     uzmtp_want_write_fn want_write;
     uzmtp_recv_fn on_recv;
     uzmtp_error_fn on_error;
