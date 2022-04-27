6d3f258be506b346c2cd13a10c6cda0da75eae99
thomas <thomas@altronix.com>
2022-04-27 18:47:49 -0400

    fixed fixture to also support zephyr app

--- test/integration/fixture/index.js
+++ test/integration/fixture/index.js
@@ -4,9 +4,7 @@ const exec = require("child_process").exec;
 const PORT = 33558;
 const command = process.argv[2];
 
-var i = 0;
 console.log(`Fixture running (${PORT})`);
-console.log(`Executing command: ${command}`);
 
 s.bind(`tcp://*:${PORT}`);
 s.on("message", function toUpper(id, hello, world) {
@@ -18,14 +16,17 @@ s.on("message", function toUpper(id, hello, world) {
   i++;
 });
 
-const cp = exec(`${command}`, function (e, stdout, stderr) {
-  if (e instanceof Error) {
-    console.error(e);
-    throw e;
-  } else {
-    s.close();
-    console.log(stdout);
-    console.error(stderr);
-    process.exit(cp.exitCode);
-  }
-});
+if (command) {
+  console.log(`Executing command: ${command}`);
+  const cp = exec(`${command}`, function (e, stdout, stderr) {
+    if (e instanceof Error) {
+      console.error(e);
+      throw e;
+    } else {
+      s.close();
+      console.log(stdout);
+      console.error(stderr);
+      process.exit(cp.exitCode);
+    }
+  });
+}
